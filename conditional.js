c83b8df2e0c5e2c7eb823996c538862e58fcd4e8
mostofa khan <mdmostofakhan1972@gmail.com>
2023-01-28 00:38:44 +0600

    multiple

---
+++ conditional.js
@@ -0,0 +1,40 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if iphone price is less then my budget. I will buy iphone
+/**
+ * if iphone price is less then my budget
+ * i wil buy the iphone
+ * 
+ * 
+ * if (condition) {
+ * // will execute if the condition is true
+ * }
+ *
+ */
+
+if (iphonePrice < myBudget) {
+    // console.log('i phone kine futani marbo');
+}
+
+var chickenPrice = 5000;
+var myMoney = 444;
+
+// if (chickenPrice <= myMoney) {
+//     console.log('Shamhed potato and lentils soup');
+// }
+
+// if (chickenPrice > myMoney) {
+//     console.log('Shamhed potato and lentils soup');
+// }
+
+/**
+ * if chiken er price is less than my budget
+ * i will eat chiken
+ * na hoi ; else
+ */
+if (chickenPrice < myMoney) {
+    console.log('I will eat chiken');
+ }
+else {
+    console.log('I will eat potato');
+}
---
+++ multiple-conditions.js
@@ -0,0 +1,16 @@
+var isGraduated = true;
+var salary = 50000;
+
+// if (isGraduated == true){
+//     console.log('Ese biya kore peli');
+// }
+// else {
+    // console.log('tor kopale biya nai')
+// }
+
+if (isGraduated = true && salary > 50000) {
+    console.log('tor kopale biya nai');
+}
+else {
+    console.log('ese biya kore peli');
+}
